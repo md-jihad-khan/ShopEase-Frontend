ac552ef0c2ed03e181926d65cbc1fe8fc79eca08
md-jihad-khan <mdjihadkhan9867@gmail.com>
2024-08-15 15:52:15 +0600

    created the navbar

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,20 @@
+import { Link } from "react-router-dom";
+
+const Navbar = () => {
+  return (
+    <div className="navbar sm:w-10/12 mx-auto ">
+      <div className="flex-1">
+        <Link to={"/"} className="font-bold text-3xl">
+          Shop Ease
+        </Link>
+      </div>
+      <div className="flex-none">
+        <button className="btn  bg-yellow-500 text-white font-bold">
+          Login
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
