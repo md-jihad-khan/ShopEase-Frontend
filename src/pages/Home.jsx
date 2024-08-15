54bad5722aba808324590c2042a213ede757f4c8
md-jihad-khan <mdjihadkhan9867@gmail.com>
2024-08-16 02:22:02 +0600

    implemented the api to get all products

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,38 @@
+import { useState, useEffect } from "react";
 import Footer from "../components/Footer";
 import Navbar from "../components/Navbar";
 
 const Home = () => {
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    fetch(`${import.meta.env.VITE_SERVER}/products`)
+      .then((res) => res.json())
+      .then((data) => {
+        setProducts(data);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }, []);
+
   return (
     <>
       <Navbar />
+      <div className="container mx-auto p-4 grid gap-4 grid-cols-1 md:grid-cols-2 lg:grid-cols-3">
+        {products?.map((product) => (
+          <div key={product.id} className="border p-4 rounded">
+            <img
+              src={product.image}
+              alt={product.name}
+              className="w-full h-48 object-cover mb-4"
+            />
+            <h2 className="text-lg font-bold">{product.name}</h2>
+            <p>{product.description}</p>
+            <p className="text-lg font-bold">${product.price}</p>
+          </div>
+        ))}
+      </div>
       <Footer />
     </>
   );
