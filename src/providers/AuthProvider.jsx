7356b16fa5e2a6ddb34880a01b3301f1d1646f11
md-jihad-khan <mdjihadkhan9867@gmail.com>
2024-08-16 00:33:52 +0600

    added the authprovider and skeleton page

---
+++ src/providers/AuthProvider.jsx
@@ -0,0 +1,66 @@
+import {
+  GoogleAuthProvider,
+  createUserWithEmailAndPassword,
+  onAuthStateChanged,
+  signInWithEmailAndPassword,
+  signInWithPopup,
+  signOut,
+} from "firebase/auth";
+
+import { createContext, useEffect, useState } from "react";
+
+import auth from "../firebase/firebase";
+import Skeleton from "../pages/Skeleton";
+
+export const AuthContext = createContext(null);
+
+const AuthProvider = ({ children }) => {
+  const [user, setUser] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const [reload, setReload] = useState(false);
+
+  const googleProvider = new GoogleAuthProvider();
+
+  const createUser = (email, password) => {
+    return createUserWithEmailAndPassword(auth, email, password);
+  };
+
+  const signIn = (email, password) => {
+    return signInWithEmailAndPassword(auth, email, password);
+  };
+  const googleLogin = () => {
+    return signInWithPopup(auth, googleProvider);
+  };
+
+  const logOut = async () => {
+    return signOut(auth);
+  };
+  useEffect(() => {
+    const unSubscribe = onAuthStateChanged(auth, (currentUser) => {
+      setUser(currentUser);
+      setLoading(false);
+    });
+    return () => {
+      unSubscribe();
+    };
+  }, [reload, user]);
+  const authInfo = {
+    loading,
+    setLoading,
+    user,
+    createUser,
+    logOut,
+    signIn,
+    googleLogin,
+    reload,
+    setReload,
+  };
+
+  return (
+    <AuthContext.Provider value={authInfo}>
+      {loading ? <Skeleton></Skeleton> : children}
+    </AuthContext.Provider>
+  );
+};
+
+export default AuthProvider;
