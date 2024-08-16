e3c3c390549e4c06e178d6dfd0f4f71bddf0fa43
md-jihad-khan <mdjihadkhan9867@gmail.com>
2024-08-16 12:35:16 +0600

    added the search functionality

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -6,30 +6,32 @@ const Navbar = () => {
   const { user, logOut } = useContext(AuthContext);
 
   return (
-    <div className="navbar sm:w-10/12 mx-auto ">
-      <div className="flex-1">
-        <Link to={"/"} className="font-bold text-3xl">
-          Shop <span className="text-yellow-500">Ease</span>
-        </Link>
-      </div>
-      <div className="flex-none">
-        {user ? (
-          <button
-            onClick={() => logOut()}
-            className="btn rounded-3xl px-5 border-none bg-yellow-500 text-white"
-          >
-            Logout
-          </button>
-        ) : (
-          <>
-            <Link
-              to={"/login"}
+    <div className="fixed w-full bg-white">
+      <div className="navbar  sm:w-10/12 mx-auto ">
+        <div className="flex-1">
+          <Link to={"/"} className="font-bold text-3xl">
+            Shop <span className="text-yellow-500">Ease</span>
+          </Link>
+        </div>
+        <div className="flex-none">
+          {user ? (
+            <button
+              onClick={() => logOut()}
               className="btn rounded-3xl px-5 border-none bg-yellow-500 text-white"
             >
-              Login
-            </Link>
-          </>
-        )}
+              Logout
+            </button>
+          ) : (
+            <>
+              <Link
+                to={"/login"}
+                className="btn rounded-3xl px-5 border-none bg-yellow-500 text-white"
+              >
+                Login
+              </Link>
+            </>
+          )}
+        </div>
       </div>
     </div>
   );
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,25 +1,63 @@
 import { useState, useEffect } from "react";
 import Footer from "../components/Footer";
 import Navbar from "../components/Navbar";
+import { TbFidgetSpinner } from "react-icons/tb";
+import { FaSearch } from "react-icons/fa";
 
 const Home = () => {
   const [products, setProducts] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [search, setSearch] = useState("");
+
+  const handleSearch = (e) => {
+    e.preventDefault();
+    setSearch(e.target.search.value);
+  };
 
   useEffect(() => {
-    fetch(`${import.meta.env.VITE_SERVER}/products`)
+    setLoading(true);
+    fetch(`${import.meta.env.VITE_SERVER}/products?search=${search}`)
       .then((res) => res.json())
       .then((data) => {
+        setLoading(false);
         setProducts(data);
       })
       .catch((err) => {
+        setLoading(false);
         console.log(err);
       });
-  }, []);
+  }, [search]);
 
   return (
-    <>
-      <Navbar />
-      <div className="container mx-auto p-4 grid gap-4 grid-cols-1 md:grid-cols-2 lg:grid-cols-3">
+    <div className="container mx-auto">
+      <div className="h-20 relative z-50 w-full">
+        <Navbar></Navbar>
+      </div>
+
+      <form onSubmit={handleSearch} className="w-full md:w-1/2 mx-auto px-2">
+        <label className="flex items-center border-r-0 pr-0">
+          <input
+            type="text"
+            name="search"
+            className="w-full input focus:outline-none border border-gray-300 rounded-full rounded-r-none"
+            placeholder="Search"
+          />
+          <button
+            type="submit"
+            className="btn-square rounded-r-full px-10 bg-yellow-500 text-white"
+          >
+            <FaSearch />
+          </button>
+        </label>
+      </form>
+
+      {loading && (
+        <div className="min-h-screen flex justify-center">
+          <TbFidgetSpinner className="text-3xl text-yellow-500  animate-spin m-auto" />
+        </div>
+      )}
+
+      <div className="container mx-auto p-4 grid gap-4 grid-cols-1 md:grid-cols-2 lg:grid-cols-3 ">
         {products?.map((product) => (
           <div key={product.id} className="border p-4 rounded">
             <img
@@ -34,7 +72,7 @@ const Home = () => {
         ))}
       </div>
       <Footer />
-    </>
+    </div>
   );
 };
 
