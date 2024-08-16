17a25d7b0c0ba8b16fe267d204f0074c547f07d4
md-jihad-khan <mdjihadkhan9867@gmail.com>
2024-08-16 19:04:13 +0600

    implemented the pagination

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -8,25 +8,38 @@ const Home = () => {
   const [products, setProducts] = useState([]);
   const [loading, setLoading] = useState(false);
   const [search, setSearch] = useState("");
+  const [currentPage, setCurrentPage] = useState(1);
+  const [totalPages, setTotalPages] = useState(1);
 
   const handleSearch = (e) => {
     e.preventDefault();
     setSearch(e.target.search.value);
   };
 
+  const pages = [...Array(totalPages).keys()].map((element) => element + 1);
+
+  const handlePaginationButton = (value) => {
+    setCurrentPage(value);
+  };
+
   useEffect(() => {
     setLoading(true);
-    fetch(`${import.meta.env.VITE_SERVER}/products?search=${search}`)
+    fetch(
+      `${
+        import.meta.env.VITE_SERVER
+      }/products?search=${search}&page=${currentPage}`
+    )
       .then((res) => res.json())
       .then((data) => {
         setLoading(false);
-        setProducts(data);
+        setProducts(data.products);
+        setTotalPages(data.totalPages);
       })
       .catch((err) => {
         setLoading(false);
         console.log(err);
       });
-  }, [search]);
+  }, [search, currentPage]);
 
   return (
     <div className="container mx-auto">
@@ -71,6 +84,69 @@ const Home = () => {
           </div>
         ))}
       </div>
+
+      <div className="pagination">
+        <div className="flex justify-center mt-12">
+          <button
+            disabled={currentPage === 1}
+            onClick={() => handlePaginationButton(currentPage - 1)}
+            className="px-4 py-2 mx-1 text-gray-700 disabled:text-gray-500 capitalize bg-gray-200 rounded-md disabled:cursor-not-allowed disabled:hover:bg-gray-200 disabled:hover:text-gray-500 hover:bg-yellow-500 hover:text-white"
+          >
+            <div className="flex items-center -mx-1">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="w-6 h-6 mx-1 rtl:-scale-x-100"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d="M7 16l-4-4m0 0l4-4m-4 4h18"
+                />
+              </svg>
+              <span className="mx-1">previous</span>
+            </div>
+          </button>
+          {pages.map((btnNum) => (
+            <button
+              onClick={() => handlePaginationButton(btnNum)}
+              key={btnNum}
+              className={`hidden ${
+                currentPage === btnNum ? "bg-yellow-500 text-white" : ""
+              } px-4 py-2 mx-1 transition-colors duration-300 transform rounded-md sm:inline hover:bg-yellow-500 hover:text-white`}
+            >
+              {btnNum}
+            </button>
+          ))}
+          <button
+            disabled={currentPage === totalPages}
+            onClick={() => handlePaginationButton(currentPage + 1)}
+            className="px-4 py-2 mx-1 text-gray-700 transition-colors duration-300 transform bg-gray-200 rounded-md hover:bg-yellow-500 disabled:hover:bg-gray-200 disabled:hover:text-gray-500 hover:text-white disabled:cursor-not-allowed disabled:text-gray-500"
+          >
+            <div className="flex items-center -mx-1">
+              <span className="mx-1">Next</span>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="w-6 h-6 mx-1 rtl:-scale-x-100"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d="M17 8l4 4m0 0l-4 4m4-4H3"
+                />
+              </svg>
+            </div>
+          </button>
+        </div>
+      </div>
+
       <Footer />
     </div>
   );
